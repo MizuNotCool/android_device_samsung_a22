b15326d3a2d72c7b66974fd7d03f1c55f2cd0d6e
ItzKaguya <106611054+MizuNotCool@users.noreply.github.com>
2022-12-11 12:46:11 +0800

    a22: Initial LineageOS Device Tree

--- lineage_a22.mk
+++ lineage_a22.mk
@@ -1,18 +1,45 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2340
+
+# Inherit from lava device makefile
+$(call inherit-product, device/samsung/a22/device.mk)
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Lineage stuff
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from a22 device
-$(call inherit-product, $(LOCAL_PATH)/device.mk)
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
-PRODUCT_BRAND := samsung
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a22
-PRODUCT_MANUFACTURER := samsung
 PRODUCT_NAME := lineage_a22
-PRODUCT_MODEL := SM-A225M
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 TARGET_VENDOR := samsung
